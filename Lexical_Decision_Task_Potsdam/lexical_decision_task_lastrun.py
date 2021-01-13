035d5706cdf90747ba1ff1b8407fb765d59b2002
giorgio-p <info.giorgiopapitto@gmail.com>
2021-01-13 13:04:00 +0100

    minor changes
    reduced the number of columns in xlsx

--- Lexical_Decision_Task_Potsdam/lexical_decision_task_lastrun.py
+++ Lexical_Decision_Task_Potsdam/lexical_decision_task_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2020.2.10),
-    on Wed Jan 13 11:12:56 2021
+    on Wed Jan 13 12:57:30 2021
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
